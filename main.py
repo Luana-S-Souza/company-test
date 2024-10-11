3e5e5a533cf89b09f52deed71a0bf835cb07156f
Luana-S-Souza <luana.a.s.silva1@gmail.com>
2024-10-11 11:06:50 -0300

    Atualizando requirements.txt

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s', 
     datefmt='%Y-%m-%d %H:%M:%S', 
     handlers=[
-        logging.FileHandler("app.log", encoding="utf-8"),
+        logging.FileHandler("robot.log", encoding="utf-8"),
         logging.StreamHandler() 
     ]
 )
